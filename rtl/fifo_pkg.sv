8daff2693d6213eb8235e0422e882af3dc2a4fcf
Daniel Niu <daniel.n@wustl.edu>
2026-06-03 14:38:11 +0800

    added package & sequence item

---
+++ rtl/fifo_pkg.sv
@@ -0,0 +1,8 @@
+package fifo_pkg;
+
+    parameter int DEPTH = 128;
+    parameter int DATA_WIDTH = 32;
+    parameter int ALMOST_FULL_THRESHOLD = 112;
+    parameter int ALMOST_EMPTY_THRESHOLD = 16;
+
+endpackage
---
+++ tb/agent/fifo_seq_item.sv
@@ -0,0 +1,51 @@
+class fifo_write_seq_item extends uvm_sequence_item;
+
+    rand logic [DATA_WIDTH-1:0] wr_data;
+
+    logic full;
+    logic almost_full;
+    logic [$clog2(DEPTH):0] count;
+
+    `uvm_object_utils_begin(fifo_write_seq_item)
+        `uvm_field_int(wr_data,     UVM_ALL_ON)
+        `uvm_field_int(full,        UVM_ALL_ON)
+        `uvm_field_int(almost_full, UVM_ALL_ON)
+        `uvm_field_int(count,       UVM_ALL_ON)
+    `uvm_object_utils_end
+
+    function new(string name = "fifo_write_seq_item");
+        super.new(name);
+    endfunction
+
+    // custom method for printing debug statement
+    function string convert2string();
+        return $sformatf("FIFO write transaction: data = %d, count = %d", wr_data, count);
+    endfunction
+
+endclass
+
+
+class fifo_read_seq_item extends uvm_sequence_item;
+
+    logic [DATA_WIDTH-1:0] rd_data;
+    logic empty;
+    logic almost_empty;
+    logic [$clog2(DEPTH):0] count;
+
+    `uvm_object_utils_begin(fifo_read_seq_item)
+        `uvm_field_int(rd_data,      UVM_ALL_ON)
+        `uvm_field_int(empty,        UVM_ALL_ON)
+        `uvm_field_int(almost_empty, UVM_ALL_ON)
+        `uvm_field_int(count,        UVM_ALL_ON)
+    `uvm_object_utils_end
+
+    function new(string name = "fifo_read_seq_item");
+        super.new(name);
+    endfunction
+
+    // custom method for printing debug statement
+    function string convert2string();
+        return $sformatf("FIFO read transaction: data = %d, count = %d", rd_data, count);
+    endfunction
+    
+endclass
