88eb908aa03f63e352353c38887b631ff297bcb2
Daniel Niu <daniel.n@wustl.edu>
2026-06-07 11:03:52 +0800

    added environment

---
+++ tb/env/fifo_env.sv
@@ -0,0 +1,29 @@
+class fifo_env extends uvm_env;
+    `uvm_component_utils(fifo_env)
+
+    fifo_wr_agent   wr_agent;
+    fifo_rd_agent   rd_agent;
+    fifo_coverage   cover_inst;
+    fifo_scoreboard scoreboard;
+
+    function new(string name = "fifo_env", uvm_component parent = null);
+        super.new(name, parent);
+    endfunction
+
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        wr_agent   = fifo_wr_agent::type_id::create("wr_agent", this);
+        rd_agent   = fifo_rd_agent::type_id::create("rd_agent", this);
+        cover_inst = fifo_coverage::type_id::create("cover_inst", this);
+        scoreboard = fifo_scoreboard::type_id::create("scoreboard", this);
+    endfunction
+
+    function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+        wr_agent.fifo_wr_ap.connect(cover_inst.wr_imp);
+        rd_agent.fifo_rd_ap.connect(cover_inst.rd_imp);
+        wr_agent.fifo_wr_ap.connect(scoreboard.wr_imp);
+        rd_agent.fifo_rd_ap.connect(scoreboard.rd_imp);
+    endfunction
+
+endclass
