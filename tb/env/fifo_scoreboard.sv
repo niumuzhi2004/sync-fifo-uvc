52c1f3a1ba1535488c45cca37611b5c950a30912
Daniel Niu <daniel.n@wustl.edu>
2026-06-09 22:39:25 +0800

    solved scoreboard concurrent read & write issue

--- tb/env/fifo_scoreboard.sv
+++ tb/env/fifo_scoreboard.sv
@@ -7,8 +7,13 @@ class fifo_scoreboard extends uvm_scoreboard;
 
     logic [DATA_WIDTH-1:0] queue [$];
     logic [DATA_WIDTH-1:0] last_rd_data = 0;
-    time wr_time_queue[$];
-    int pending_pop = 0;
+    time  wr_time_queue[$];
+
+    // collect all simultaneous transactions, then process reads before writes
+    fifo_write_seq_item wr_bucket[$];
+    fifo_read_seq_item  rd_bucket[$];
+    time bucket_time;
+    bit  bucket_valid = 0;
 
     function new(string name = "fifo_scoreboard", uvm_component parent = null);
         super.new(name, parent);
@@ -21,18 +26,47 @@ class fifo_scoreboard extends uvm_scoreboard;
     endfunction
 
     function void write_wr(fifo_write_seq_item item);
+        if (bucket_valid && $time != bucket_time) flush_bucket();
+        bucket_time  = $time;
+        bucket_valid = 1;
+        wr_bucket.push_back(item);
+    endfunction
+
+    function void write_rd(fifo_read_seq_item item);
+        if (bucket_valid && $time != bucket_time) flush_bucket();
+        bucket_time  = $time;
+        bucket_valid = 1;
+        rd_bucket.push_back(item);
+    endfunction
+
+    function void flush_bucket();
+        foreach (rd_bucket[i])
+            process_rd(rd_bucket[i]);
+        foreach (wr_bucket[i]) 
+            process_wr(wr_bucket[i]);
+        rd_bucket.delete();
+        wr_bucket.delete();
+        bucket_valid = 0;
+    endfunction
+
+    function void check_phase(uvm_phase phase);
+        super.check_phase(phase);
+        if (bucket_valid) flush_bucket();
+    endfunction
+
+    function void process_wr(fifo_write_seq_item item);
         logic full_passed, almost_full_passed, count_passed;
 
         full_passed        = item.full == (queue.size() == DEPTH);
         almost_full_passed = item.almost_full == (queue.size() >= ALMOST_FULL_THRESHOLD);
-        count_passed       = (item.count == queue.size() - pending_pop);
+        count_passed       = (item.count == queue.size());
 
         `uvm_info("SCORE", $sformatf(
                     "write_wr called: queue.size=%0d, item.full=%0d, item.count=%0d",
                     queue.size(), item.full, item.count), UVM_LOW)
 
         if (queue.size() < DEPTH) begin
-            wr_time_queue.push_back($time);
+            wr_time_queue.push_back(bucket_time);
             queue.push_back(item.wr_data);
         end
         else begin
@@ -55,13 +89,13 @@ class fifo_scoreboard extends uvm_scoreboard;
 
     endfunction
 
-    function void write_rd(fifo_read_seq_item item);
+    function void process_rd(fifo_read_seq_item item);
         logic [DATA_WIDTH-1:0] exp_result;
         logic fifo_passed, empty_passed, almost_empty_passed, count_passed;
 
-        // check if read and write arrive simultaneously
+        // check if this read & write are simultaneous 
         bit is_simultaneous = (wr_time_queue.size() > 0 && wr_time_queue[0] == item.capture_time);
-        if (is_simultaneous) 
+        if (is_simultaneous)
             wr_time_queue.delete(0);
 
         if (is_simultaneous) begin // simultaneous reads and writes
@@ -86,12 +120,8 @@ class fifo_scoreboard extends uvm_scoreboard;
             exp_result = last_rd_data;
             `uvm_info("SCORE", $sformatf("Read data holding previous value %d while FIFO is empty.", item.rd_data), UVM_LOW)
         end else begin
-            if (is_simultaneous) 
-                pending_pop++;
             exp_result   = queue.pop_front();
             last_rd_data = exp_result;
-            if (pending_pop > 0) 
-                pending_pop--;
         end
 
         fifo_passed = (item.rd_data == exp_result);
@@ -116,10 +146,13 @@ class fifo_scoreboard extends uvm_scoreboard;
     endfunction
 
     function void reset();
+        if (bucket_valid) flush_bucket();
         queue.delete();
         last_rd_data = 0;
         wr_time_queue.delete();
-        pending_pop = 0;
+        wr_bucket.delete();
+        rd_bucket.delete();
+        bucket_valid = 0;
     endfunction
 
 endclass
